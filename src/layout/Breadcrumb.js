312155084d928e3a5b818d504d9fb12f1ca5bf36
zhaoshaoyong1970 <zhaoshaoyong1970@foxmail.com>
2021-05-08 18:33:22 +0800

    no message

---
+++ src/layout/Breadcrumb.js
@@ -0,0 +1,13 @@
+import { Breadcrumb } from 'antd';
+
+const MainBreadcrumb = function () {
+  return (
+    <Breadcrumb style={{ margin: '16px 0' }}>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>List</Breadcrumb.Item>
+      <Breadcrumb.Item>App</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
+
+export default MainBreadcrumb;
